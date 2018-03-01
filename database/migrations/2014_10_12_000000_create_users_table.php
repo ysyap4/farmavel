289721a0299ba7ea4fa3bb5e52c10529e65c5d84
Yap <yoongseng_yap@outlook.com>
2018-03-01 13:15:47 +0800

    make it better

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,13 +18,10 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->string('password');
-            $table->string('provider');
-            $table->string('provider_id');
             $table->rememberToken();
             $table->timestamps();
         });
-}
-
+    }
 
     /**
      * Reverse the migrations.
