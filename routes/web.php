f35dfb4a0efbbe7ee3db3545d4378ec198211722
Yap <yoongseng_yap@outlook.com>
2018-03-02 19:05:31 +0800

    make it better

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,11 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('app/{name}', function($name) {
+  return redirect('/public/app/' . $name);
+})->where('name', '[A-Za-z0-9\/\.\-]+');
+
+Route::get('assets/{name}', function($name) {
+  return redirect('/public/assets/' . $name);
+})->where('name', '[A-Za-z0-9\/\.\-]+');
