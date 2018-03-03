242111fe2fa21a271dad054adc9b7aee80047b85
Yap <yoongseng_yap@outlook.com>
2018-03-03 16:49:44 +0800

    make it better

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -72,16 +72,6 @@
     </head>
     <body>
         <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @if (Auth::check())
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ url('/login') }}">Login</a>
-                        <a href="{{ url('/register') }}">Register</a>
-                    @endif
-                </div>
-            @endif
 
             <div class="page-not-found-modal">
                 <div class="title m-b-md">
@@ -89,13 +79,18 @@
                 </div>
 
                 <div class="links">
-                    @if (Auth::check())
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ url('/login') }}">Login</a>
-                        <a href="{{ url('/register') }}">Register</a>
+                    @if (Route::has('login'))
+                        <div class="top-right links">
+                            @if (Auth::check())
+                                <a href="{{ url('/home') }}">Home</a>
+                            @else
+                                <a href="{{ url('/login') }}">Login</a>
+                                <a href="{{ url('/register') }}">Register</a>
+                            @endif
+                        </div>
                     @endif
                 </div>
+                
             </div>
         </div>
     </body>
