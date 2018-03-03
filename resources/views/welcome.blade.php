52d0526c34e186a429e5dee726d6eccf821b912b
Yap <yoongseng_yap@outlook.com>
2018-03-03 17:03:39 +0800

    make it better

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -80,17 +80,15 @@
 
                 <div class="links">
                     @if (Route::has('login'))
-                        <div class="top-right links">
                             @if (Auth::check())
                                 <a href="{{ url('/home') }}">Home</a>
                             @else
                                 <a href="{{ url('/login') }}">Login</a>
                                 <a href="{{ url('/register') }}">Register</a>
                             @endif
-                        </div>
                     @endif
                 </div>
-                </div>
+            </div>
             </div>
         </div>
     </body>
