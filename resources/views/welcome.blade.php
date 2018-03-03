55ce2b3de30bda3cfc824a5e2dff346fd60a47c3
Yap <yoongseng_yap@outlook.com>
2018-03-03 17:40:00 +0800

    make it better

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,19 +5,13 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Farmavel</title>
 
         <!-- Fonts -->
-        <link href='https://fonts.googleapis.com/css?family=Roboto:400,100,100italic,300,300italic,400italic,500,500italic,700,700italic,900italic,900&subset=latin,greek,greek-ext,vietnamese,cyrillic-ext,latin-ext,cyrillic' rel='stylesheet' type='text/css'>
-
-        <link rel="icon" type="image/png" sizes="16x16" href="{{URL::asset('admin/assets/img/favicon-16x16.png')}}">
-        <link rel="icon" type="image/png" sizes="32x32" href="{{URL::asset('admin/assets/img/favicon-32x32.png')}}">
-        <link rel="icon" type="image/png" sizes="96x96" href="{{URL::asset('admin/assets/img/favicon-96x96.png')}}">
-
-        <link rel="stylesheet" href="{{URL::asset('admin/app/404.css')}}">
+        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
 
         <!-- Styles -->
-        <!-- <style>
+        <style>
             html, body {
                 background-color: #fff;
                 color: #636b6f;
@@ -68,28 +62,28 @@
             .m-b-md {
                 margin-bottom: 30px;
             }
-        </style> -->
+        </style>
     </head>
 
     <body>
-        <div class="page-not-found-modal">
-            <h1>
-                Farmavel
-            </h1>
+        <div class="flex-center position-ref full-height">
+            <div class="content">
+                <div class="title m-b-md">
+                    Farmavel
+                </div>
 
-
+                <div class="links">
                     @if (Route::has('login'))
-                            @if (Auth::check())
-                                <a href="{{ url('/home') }}">Home</a>
-                            @else
-                                <a href="{{ url('/login') }}">Login</a>
-                                <a href="{{ url('/register') }}">Register</a>
-                            @endif
+                        @if (Auth::check())
+                            <a href="{{ url('/home') }}">Home</a>
+                        @else
+                            <a href="{{ url('/login') }}">Login</a>
+                            <a href="{{ url('/register') }}">Register</a>
+                        @endif
                     @endif
-
-
+                </div>
+            </div>
         </div>
+        
     </body>
-
 </html>
-
