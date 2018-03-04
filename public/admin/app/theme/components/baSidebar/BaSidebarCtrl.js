84280f68579cec03a2f256c01fe3a9ba7e9465ca
Yap <yoongseng_yap@outlook.com>
2018-03-04 21:58:02 +0800

    make it better

--- public/admin/app/theme/components/baSidebar/BaSidebarCtrl.js
+++ public/admin/app/theme/components/baSidebar/BaSidebarCtrl.js
@@ -6,25 +6,39 @@
   'use strict';
 
   angular.module('BlurAdmin.theme.components')
-    .controller('BaSidebarCtrl', BaSidebarCtrl);
+  .controller('BaSidebarCtrl', BaSidebarCtrl);
 
-  /** @ngInject */
-  function BaSidebarCtrl($scope, baSidebarService) {
+/** @ngInject */
+function BaSidebarCtrl($scope, baSidebarService, $window, baConfig) {
 
     $scope.menuItems = baSidebarService.getMenuItems();
+    $scope.menuItemsAccess = [];
+    var jsonMenu = JSON.parse($window.sessionStorage.menus); // JSON from Service
+
+    angular.forEach($scope.menuItems, function (baSideBarMenu) {
+        angular.forEach(jsonMenu, function (accessMenu) {
+            if (accessMenu.MenuName === baSideBarMenu.name) {
+                $scope.menuItemsAccess.push(baSideBarMenu)
+                return;
+            }
+        })
+    })
+
+    $scope.menuItems = $scope.menuItemsAccess;
+
     $scope.defaultSidebarState = $scope.menuItems[0].stateRef;
 
     $scope.hoverItem = function ($event) {
-      $scope.showHoverElem = true;
-      $scope.hoverElemHeight =  $event.currentTarget.clientHeight;
-      var menuTopValue = 66;
-      $scope.hoverElemTop = $event.currentTarget.getBoundingClientRect().top - menuTopValue;
+        $scope.showHoverElem = true;
+        $scope.hoverElemHeight = $event.currentTarget.clientHeight;
+        var menuTopValue = 60;
+        $scope.hoverElemTop = $event.currentTarget.getBoundingClientRect().top - menuTopValue;
     };
 
     $scope.$on('$stateChangeSuccess', function () {
-      if (baSidebarService.canSidebarBeHidden()) {
-        baSidebarService.setMenuCollapsed(true);
-      }
+        if (baSidebarService.canSidebarBeHidden()) {
+            baSidebarService.setMenuCollapsed(true);
+        }
     });
-  }
+}
 })();
