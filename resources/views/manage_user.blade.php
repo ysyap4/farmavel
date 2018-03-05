63f514268e21c3ad28499381b4f5cb71ff17a722
Yap <yoongseng_yap@outlook.com>
2018-03-06 00:28:17 +0800

    make it better

--- resources/views/manage_user.blade.php
+++ resources/views/manage_user.blade.php
@@ -210,7 +210,7 @@
                     <h2>Manage User</h2>
                     <ol class="breadcrumb">
                         <li>
-                            <a href="index.html">Home</a>
+                            <a href="{{ url('/home') }}">Home</a>
                         </li>
                         <li>
                             <a>Manage</a>
