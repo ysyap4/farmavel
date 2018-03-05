a16b04ed3e7ca5802d58d766729ac5b5423a753b
Yap <yoongseng_yap@outlook.com>
2018-03-05 16:29:28 +0800

    make it better

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -326,9 +326,15 @@
 
 
                 <li>
-                    <a href="{{ url('/') }}">
+                    <a href="{{ route('logout') }}"
+                        onclick="event.preventDefault();
+                                  document.getElementById('logout-form').submit();">
                         <i class="fa fa-sign-out"></i> Log out
                     </a>
+
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                        {{ csrf_field() }}
+                    </form>
                 </li>
             </ul>
 
