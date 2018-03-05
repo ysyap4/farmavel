112f4bc353d4467036ecafc83604736ff3610a3c
Yap <yoongseng_yap@outlook.com>
2018-03-05 11:11:32 +0800

    make it better

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -32,7 +32,7 @@
             {{ csrf_field() }}
 
                 <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                    <label for="email" class="col-sm-2 control-label">Email</label>
+                    <!--<label for="email" class="col-sm-2 control-label">Email</label>-->
                     
                     <div class="col-sm-10">
                         <input type="email" class="form-control" id="email" name="email" placeholder="Email" value="{{ old('email') }}" required autofocus>
@@ -46,7 +46,7 @@
                 </div>
 
                 <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                    <label for="password" class="col-sm-2 control-label">Password</label>
+                    <!--<label for="password" class="col-sm-2 control-label">Password</label>-->
 
                     <div class="col-sm-10">
                         <input type="password" class="form-control" id="password" name="password" placeholder="Password" required>
@@ -70,8 +70,8 @@
     </div>
 
     <!-- Mainly scripts -->
-    <script src="{{URL::asset('inspinia-master/assets/js/jquery-2.1.1.js')}}"></script>
-    <script src="{{URL::asset('inspinia-master/assets/js/bootstrap.min.js')}}"></script>
+    <script src="{{asset('inspinia-master/assets/js/jquery-2.1.1.js')}}"></script>
+    <script src="{{asset('inspinia-master/assets/js/bootstrap.min.js')}}"></script>
 
 </body>
 
