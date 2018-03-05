97cc3f3fa3b46cd44f93ee5658871296c43383d0
Yap <yoongseng_yap@outlook.com>
2018-03-05 22:46:33 +0800

    make it better

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -84,12 +84,12 @@
         <nav class="navbar navbar-static-top white-bg" role="navigation" style="margin-bottom: 0">
         <div class="navbar-header">
             <a class="navbar-minimalize minimalize-styl-2 btn btn-primary " href="#"><i class="fa fa-bars"></i> </a>
+<span class="m-r-sm text-muted welcome-message">Welcome to Farmavel Administration</span>
         </div>
-            <ul class="nav navbar-top-links">
+            <ul class="nav navbar-top-links navbar-right">
                 <li>
                     <span class="m-r-sm text-muted welcome-message">Welcome to Farmavel Administration</span>
                 </li>
-                <div class="navbar-right">
                 <li class="dropdown">
                     <a class="dropdown-toggle count-info" data-toggle="dropdown" href="#">
                         <i class="fa fa-envelope"></i>  <span class="label label-warning">16</span>
@@ -198,7 +198,6 @@
                         {{ csrf_field() }}
                     </form>
                 </li>
-                </div>
             </ul>
 
         </nav>
