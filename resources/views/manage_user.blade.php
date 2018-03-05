d875c850589f77e34952d3928bcc3470f3975b26
Yap <yoongseng_yap@outlook.com>
2018-03-05 19:48:34 +0800

    make it better

--- resources/views/manage_user.blade.php
+++ resources/views/manage_user.blade.php
@@ -356,7 +356,8 @@
 
                 </div>
             </div>
-        
+            
+        <div class="wrapper wrapper-content animated fadeInRight">
             <div class="row">
             <div class="col-lg-12">
             <div class="ibox float-e-margins">
@@ -841,7 +842,7 @@
 
         </div>
         </div>
-</div>
+
 
 
     <!-- Mainly scripts -->
