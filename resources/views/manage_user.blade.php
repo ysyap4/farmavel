998fca4cbf9ad355f92d29617b526b0e0f121084
Yap <yoongseng_yap@outlook.com>
2018-03-07 23:41:17 +0800

    make it better

--- resources/views/manage_user.blade.php
+++ resources/views/manage_user.blade.php
@@ -361,7 +361,7 @@
             var oTable = $('#editable').dataTable();
 
             /* Apply the jEditable handlers to the table */
-            oTable.$('td').editable( '{{URL::asset('node_modules/jquery-jeditable/save.php')}}', {
+            oTable.$('td').editable( '{{URL::asset('node_modules/jquery-jeditable/save.blade.php')}}', {
                 "callback": function( sValue, y ) {
                     var aPos = oTable.fnGetPosition( this );
                     oTable.fnUpdate( sValue, aPos[0], aPos[1] );
