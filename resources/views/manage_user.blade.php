18ef89f7a6b6aa82c5b598e3da16651bc1ed71a2
Yap <yoongseng_yap@outlook.com>
2018-03-08 17:53:38 +0800

    make it better

--- resources/views/manage_user.blade.php
+++ resources/views/manage_user.blade.php
@@ -353,7 +353,7 @@
                 responsive: true,
                 "dom": 'T<"clear">lfrtip',
                 "tableTools": {
-                    "sSwfPath": "js/plugins/dataTables/swf/copy_csv_xls_pdf.swf"
+                    "sSwfPath": "{{URL::asset('inspinia-master/assets/js/plugins/dataTables/swf/copy_csv_xls_pdf.swf')}}"
                 }
             });
 
