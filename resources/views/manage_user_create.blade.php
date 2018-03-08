1a58af96031c58f255a26b0c26c37316c080e7a6
Yap <yoongseng_yap@outlook.com>
2018-03-08 23:32:52 +0800

    make it better

--- resources/views/manage_user_create.blade.php
+++ resources/views/manage_user_create.blade.php
@@ -238,7 +238,7 @@
                             </div>
                         </div>
                         <div class="ibox-content">
-                            <form method="POST" class="form-horizontal" action="{{ URL::route ('manage_user_create_process')}}">
+                            <form method="POST" class="form-horizontal" action="">
                                 <input type="hidden" name="_token" value="{{ csrf_token() }}">
 
                                 <div class="form-group @if ($errors ->has ('name')) has-error @endif"><label class="col-sm-2 control-label">Full Name</label>
