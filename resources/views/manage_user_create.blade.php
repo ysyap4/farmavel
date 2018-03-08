da4aff08a380b29b360fb182599635346ec0a251
Yap <yoongseng_yap@outlook.com>
2018-03-08 23:54:23 +0800

    make it better

--- resources/views/manage_user_create.blade.php
+++ resources/views/manage_user_create.blade.php
@@ -241,19 +241,10 @@
                             <form method="POST" class="form-horizontal" action="{{ URL::route ('manage_user_create_process')}}">
                                 <input type="hidden" name="_token" value="{{ csrf_token() }}">
 
-                                <div class="form-group"><label class="col-sm-2 control-label">Full Name</label>
+                                <div class="form-group"><label class="col-sm-2 control-label">Normal</label>
 
-                                    <div class="col-sm-10">
-                                        <input type="text" id="name" class="form-control" name="name" placeholder="Full Name" value="{{ Input::old('name')}}">
-                                        @if ($errors->has('name'))
-                                            <p class="help-block">{{$errors ->first('name')}}</p>
-                                        @endif
-                                    </div>
+                                    <div class="col-sm-10"><input type="text" class="form-control"></div>
                                 </div>
-
-                                <div class="hr-line-dashed"></div>
-
-
 
                                 <div class="hr-line-dashed"></div>
 
