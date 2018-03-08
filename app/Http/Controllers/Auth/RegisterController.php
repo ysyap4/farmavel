5f5ae0b474f7deacc246e530a9108f3c9e8a3e74
Yap <yoongseng_yap@outlook.com>
2018-03-08 22:51:28 +0800

    make it better

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -50,7 +50,7 @@ class RegisterController extends Controller
         return Validator::make($data, [
             'name' => 'required|string|max:255',
             'email' => 'required|string|email|max:255|unique:users',
-            //'phone' => 'required|min:10'
+            'phone' => 'required|min:10',
             'password' => 'required|string|min:6|confirmed',
         ]);
     }
@@ -66,7 +66,7 @@ class RegisterController extends Controller
         return User::create([
             'name' => $data['name'],
             'email' => $data['email'],
-            //'phone' => $data['phone'],
+            'phone' => $data['phone'],
             'password' => bcrypt($data['password']),
         ]);
     }
