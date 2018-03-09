be6c9a8e6c22105409800a6dca6a7dc67c7e9573
Yap <yoongseng_yap@outlook.com>
2018-03-09 16:19:43 +0800

    make it better

--- resources/views/manage_user_index.blade.php
+++ resources/views/manage_user_index.blade.php
@@ -240,7 +240,7 @@
                     </div>
                     <div class="ibox-content">
                         <a class="btn btn-primary" href="{{URL::route('manage_user_create')}}">Add</a>
-                        <a class="btn btn-primary" onClick="manage_user_edit()">Edit</a>
+                        <button class="btn btn-primary" onClick="manage_user_edit()">Edit</button>
                         <button type="submit" class="btn btn-primary" id="manage_user_delete" onClick="manage_user_delete()">Delete</button>
                     <form method="GET" name="get_checkbox">
                     <table class="table table-striped table-bordered table-hover dataTables-example" id="allBlogs">
