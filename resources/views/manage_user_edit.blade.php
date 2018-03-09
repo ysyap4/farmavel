91c62994f170f18342223e165bc9e9950ab39444
Yap <yoongseng_yap@outlook.com>
2018-03-09 14:48:33 +0800

    make it better

--- resources/views/manage_user_edit.blade.php
+++ resources/views/manage_user_edit.blade.php
@@ -219,7 +219,7 @@
 
     <div class="wrapper wrapper-content animated fadeInRight">
         <div class="row">
-    <form method="POST" class="form-horizontal" name="manage_user_edit_process" action="{{ URL::route ('manage_user_edit_process')}}">
+    <form method="POST" class="form-horizontal" name="manage_user_edit_process" id="manage_user_edit_process" action="{{ URL::route ('manage_user_edit_process')}}">
     <input type="hidden" name="_token" value="{{ csrf_token() }}">
     @for ($i=0; $i < sizeof($edit_selected_user); $i++)
                 <div class="col-lg-6">
