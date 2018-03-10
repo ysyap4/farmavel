82290a08ab43d61bdef316f537a7f631bc3daea2
Yap <yoongseng_yap@outlook.com>
2018-03-10 14:47:07 +0800

    make it better

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -58,8 +58,8 @@
                     <a href="{{ url('/home') }}"><i class="fa fa-table"></i> <span class="nav-label">Manage</span> <span class="fa arrow"></span></a>
                     <ul class="nav nav-second-level">
                         <li><a href="{{URL::route('manage_user_index')}}">User</a></li>
-                        <li><a href="dashboard_2.html">Drugs</a></li>
-                        <li><a href="dashboard_3.html">Illegal Drug Report</a></li>
+                        <li><a href="dashboard_2.html">Medicine</a></li>
+                        <li><a href="dashboard_3.html">Illegal Medicine Report</a></li>
                         <li><a href="dashboard_4_1.html">Appointment</a></li>
                     </ul>
                 </li>
