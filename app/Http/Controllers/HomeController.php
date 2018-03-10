483b8f10b2c29fa7a83656b855fbd7b3cf6aacf3
Yap <yoongseng_yap@outlook.com>
2018-03-10 12:25:30 +0800

    make it better

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use View;
+use App\Model\users;
+
 class HomeController extends Controller
 {
     /**
@@ -23,6 +26,8 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $user = users::all();
+
+        return View::make('home',array('user' => $user));
     }
 }
