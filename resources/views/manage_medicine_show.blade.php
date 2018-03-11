294b5aa8470c248f07f2a6a22ad80c93beded581
Yap <yoongseng_yap@outlook.com>
2018-03-11 11:45:55 +0800

    make it better

--- resources/views/manage_medicine_show.blade.php
+++ resources/views/manage_medicine_show.blade.php
@@ -100,18 +100,7 @@
                             </div>
                         </li>
                         <li class="divider"></li>
-                        <li>
-                            <div class="dropdown-messages-box">
-                                <a href="profile.html" class="pull-left">
-                                    <img alt="image" class="img-circle" src="{{URL::asset('inspinia-master/assets/img/a4.jpg')}}">
-                                </a>
-                                <div class="media-body ">
-                                    <small class="pull-right text-navy">NEW medicine</small>
-                                    <strong> {{$lastest_med->med_name}} </strong> is added as <strong> {{$lastest_med->med_number}} </strong>. <br>
-                                    <small class="text-muted">{{$lastest_med->med_category}} category</small>
-                                </div>
-                            </div>
-                        </li>
+
                         <li class="divider"></li>
                         <li>
                             <div class="dropdown-messages-box">
