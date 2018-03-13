f656590f7c2e2dedd3bba2ab313aa3c1c8301b85
Yap <yoongseng_yap@outlook.com>
2018-03-13 22:24:33 +0800

    make it better

--- resources/views/graph_alltime_index.blade.php
+++ resources/views/graph_alltime_index.blade.php
@@ -172,6 +172,7 @@
             </div>
         </div>
         <div class="wrapper wrapper-content animated fadeInRight">
+            
             <div class="row">
                 <div class="col-lg-6">
                     <div class="ibox float-e-margins">
@@ -335,89 +336,6 @@
 
     <script type="text/javascript">
     $(function () {
-        var lineData = {
-            labels: ["January", "February", "March", "April", "May", "June", "July"],
-            datasets: [
-                {
-                    label: "Example dataset",
-                    fillColor: "rgba(220,220,220,0.5)",
-                    strokeColor: "rgba(220,220,220,1)",
-                    pointColor: "rgba(220,220,220,1)",
-                    pointStrokeColor: "#fff",
-                    pointHighlightFill: "#fff",
-                    pointHighlightStroke: "rgba(220,220,220,1)",
-                    data: [65, 59, 80, 81, 56, 55, 40]
-                },
-                {
-                    label: "Example dataset",
-                    fillColor: "rgba(26,179,148,0.5)",
-                    strokeColor: "rgba(26,179,148,0.7)",
-                    pointColor: "rgba(26,179,148,1)",
-                    pointStrokeColor: "#fff",
-                    pointHighlightFill: "#fff",
-                    pointHighlightStroke: "rgba(26,179,148,1)",
-                    data: [28, 48, 40, 19, 86, 27, 90]
-                }
-            ]
-        };
-    
-        var lineOptions = {
-            scaleShowGridLines: true,
-            scaleGridLineColor: "rgba(0,0,0,.05)",
-            scaleGridLineWidth: 1,
-            bezierCurve: true,
-            bezierCurveTension: 0.4,
-            pointDot: true,
-            pointDotRadius: 4,
-            pointDotStrokeWidth: 1,
-            pointHitDetectionRadius: 20,
-            datasetStroke: true,
-            datasetStrokeWidth: 2,
-            datasetFill: true,
-            responsive: true,
-        };
-    
-    
-        var ctx = document.getElementById("lineChart").getContext("2d");
-        var myNewChart = new Chart(ctx).Line(lineData, lineOptions);
-    
-        var barData = {
-            labels: ["January", "February", "March", "April", "May", "June", "July"],
-            datasets: [
-                {
-                    label: "My First dataset",
-                    fillColor: "rgba(220,220,220,0.5)",
-                    strokeColor: "rgba(220,220,220,0.8)",
-                    highlightFill: "rgba(220,220,220,0.75)",
-                    highlightStroke: "rgba(220,220,220,1)",
-                    data: [65, 59, 80, 81, 56, 55, 40]
-                },
-                {
-                    label: "My Second dataset",
-                    fillColor: "rgba(26,179,148,0.5)",
-                    strokeColor: "rgba(26,179,148,0.8)",
-                    highlightFill: "rgba(26,179,148,0.75)",
-                    highlightStroke: "rgba(26,179,148,1)",
-                    data: [28, 48, 40, 19, 86, 27, 90]
-                }
-            ]
-        };
-    
-        var barOptions = {
-            scaleBeginAtZero: true,
-            scaleShowGridLines: true,
-            scaleGridLineColor: "rgba(0,0,0,.05)",
-            scaleGridLineWidth: 1,
-            barShowStroke: true,
-            barStrokeWidth: 2,
-            barValueSpacing: 5,
-            barDatasetSpacing: 1,
-            responsive: true,
-        }
-    
-    
-        var ctx = document.getElementById("barChart").getContext("2d");
-        var myNewChart = new Chart(ctx).Bar(barData, barOptions);
     
         var polarData = [
             {
