22530ba491ca9596c82e5f84dcd506810696c759
Yap <yoongseng_yap@outlook.com>
2018-03-15 18:34:41 +0800

    make it better

--- resources/views/manage_user_edit.blade.php
+++ resources/views/manage_user_edit.blade.php
@@ -258,10 +258,10 @@
                                 <div class="form-group{{ $errors->has('type') ? ' has-error' : '' }}"><label class="col-sm-2 control-label">Type</label>
 
                                     <div class="col-sm-10">
-                                        @if ($edit_selected_user[$i]->type == "Patient")
+                                        @if ($edit_selected_user[$i]->type === 'Patient')
                                         <div class="radio"><label> <input type="radio" value="Patient" id="type[]" name="type[]" checked> Patient </label></div>
                                         <div class="radio"><label> <input type="radio" value="Admin" id="type[]" name="type[]"> Admin </label></div>
-                                        @elseif ($edit_selected_user[$i]->type == "Admin")
+                                        @elseif ($edit_selected_user[$i]->type === 'Admin')
                                         <div class="radio"><label> <input type="radio" value="Patient" id="type[]" name="type[]"> Patient </label></div>
                                         <div class="radio"><label> <input type="radio" value="Admin" id="type[]" name="type[]" checked> Admin </label></div>
                                         @endif
