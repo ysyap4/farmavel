20fdebc1aa7bc88df7afea0e23166e23d094388a
Yap <yoongseng_yap@outlook.com>
2018-03-16 00:43:08 +0800

    make it better

--- resources/views/manage_appointment_create.blade.php
+++ resources/views/manage_appointment_create.blade.php
@@ -331,8 +331,8 @@
             });
 
         $(".dial").knob({
-            'min':8:00,
-            'max':18:00,
+            'min':8,
+            'max':18,
             'step':11,
             'angleOffset':240,
             'angleArc':300
