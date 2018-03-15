abd6e3433fa2afe17f75ae70972b5d474749c1f8
Yap <yoongseng_yap@outlook.com>
2018-03-16 00:14:16 +0800

    make it better

--- resources/views/manage_appointment_create.blade.php
+++ resources/views/manage_appointment_create.blade.php
@@ -322,6 +322,7 @@
 
     <script type="text/javascript">
         $('#data_1 .input-group.date').datepicker({
+                dateFormat: 'dd/mm/yy',
                 todayBtn: "linked",
                 keyboardNavigation: false,
                 forceParse: false,
