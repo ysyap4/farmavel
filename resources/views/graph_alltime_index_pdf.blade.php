81470d71046293f4b555d2283512efb3782544b4
Yap <yoongseng_yap@outlook.com>
2018-03-15 14:45:56 +0800

    make it better

--- resources/views/graph_alltime_index_pdf.blade.php
+++ resources/views/graph_alltime_index_pdf.blade.php
@@ -29,7 +29,7 @@
                             <div ibox-tools></div>
                         </div>
                         <div class="ibox-content">
-                            <div style="width:800px; height:200;">
+                            <div style="width:800px; height:200; background-color: transparent;">
                                 <canvas id="doughnutChart" height="140"></canvas>
                             </div>
                             <h5><div class="color-box a3e1d4"></div> {{$pie_medicine_legal_count}} Legal </h5>
@@ -45,7 +45,7 @@
                             <div ibox-tools></div>
                         </div>
                         <div class="ibox-content">
-                            <div class="text-center" style="width:800px; height:200;">
+                            <div class="text-center" style="width:800px; height:200;background-color: transparent;">
                                 <canvas id="polarChart" height="140"></canvas>
                             </div>
                             <h5><div class="color-box a3e1d4"></div> {{$polar_medicine_traditional_count}} Traditional </h5>
@@ -63,7 +63,7 @@
                             <div ibox-tools></div>
                         </div>
                         <div class="ibox-content">
-                            <div style="width:800px; height:200;">
+                            <div style="width:800px; height:200; background-color: transparent;">
                                 <canvas id="radarChart"></canvas>
                             </div>
                             <table class="table-align-center">
