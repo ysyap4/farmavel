a65c81edac2cfee2fa6f13a09853e04746b3beb1
Yap <yoongseng_yap@outlook.com>
2018-03-16 14:42:36 +0800

    make it better

--- resources/views/graph_periodic_index.blade.php
+++ resources/views/graph_periodic_index.blade.php
@@ -154,7 +154,7 @@
         </div>
         <div class="row wrapper border-bottom white-bg page-heading">
             <div class="col-lg-10">
-                <h2>All-Time Charts</h2>
+                <h2>Periodic Charts</h2>
                 <ol class="breadcrumb">
                     <li>
                         <a href="{{ url('/home') }}">Home</a>
@@ -168,9 +168,54 @@
                 </ol>
             </div>
         </div>
+
         <div class="wrapper wrapper-content animated fadeInRight">
-            
-            
+            <div class="row">
+            <div class="col-lg-7">
+                <div class="ibox float-e-margins">
+                    <div class="ibox-title">
+                        <h5>Basic form <small>Simple login form example</small></h5>
+                        <div class="ibox-tools">
+                            <a class="collapse-link">
+                                <i class="fa fa-chevron-up"></i>
+                            </a>
+                            <a class="dropdown-toggle" data-toggle="dropdown" href="#">
+                                <i class="fa fa-wrench"></i>
+                            </a>
+                            <ul class="dropdown-menu dropdown-user">
+                                <li><a href="#">Config option 1</a>
+                                </li>
+                                <li><a href="#">Config option 2</a>
+                                </li>
+                            </ul>
+                            <a class="close-link">
+                                <i class="fa fa-times"></i>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="ibox-content">
+                        <div class="row">
+                            <div class="col-sm-6 b-r"><h3 class="m-t-none m-b">Sign in</h3>
+                                <p>Sign in today for more expirience.</p>
+                                <form role="form">
+                                    <div class="form-group"><label>Email</label> <input type="email" placeholder="Enter email" class="form-control"></div>
+                                    <div class="form-group"><label>Password</label> <input type="password" placeholder="Password" class="form-control"></div>
+                                    <div>
+                                        <button class="btn btn-sm btn-primary pull-right m-t-n-xs" type="submit"><strong>Log in</strong></button>
+                                        <label> <input type="checkbox" class="i-checks"> Remember me </label>
+                                    </div>
+                                </form>
+                            </div>
+                            <div class="col-sm-6"><h4>Not a member?</h4>
+                                <p>You can create an account:</p>
+                                <p class="text-center">
+                                    <a href=""><i class="fa fa-sign-in big-icon"></i></a>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
 
         <div class="footer">
