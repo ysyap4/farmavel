5484a31fb3bcaa89e9aa2ae478f0fc47f6c85a2d
Yap <yoongseng_yap@outlook.com>
2018-03-16 15:25:47 +0800

    make it better

--- resources/views/graph_periodic_index.blade.php
+++ resources/views/graph_periodic_index.blade.php
@@ -186,21 +186,18 @@
                     </div>
                     <div class="ibox-content">
                         <div class="row">
-                            <div class="col-sm-6 b-r"><h3 class="m-t-none m-b">Select information <small>to be displayed.</small> </h3>
+                            <div class="col-sm-6 b-r"><h4 class="m-t-none m-b">Select period <small>to be displayed.</small> </h4>
                                 <form role="form">
-                                    <div class="form-group"><label>Email</label> <input type="email" placeholder="Enter email" class="form-control"></div>
-                                    <div class="form-group"><label>Password</label> <input type="password" placeholder="Password" class="form-control"></div>
+                                    <div class="form-group"><label>Information</label> <input type="email" placeholder="Enter email" class="form-control"></div>
+                                    <div class="form-group"><label>Number of month(s)</label> <input type="email" placeholder="Enter email" class="form-control"></div>
+                                    <div class="form-group"><label>Start from month</label> <input type="password" placeholder="Password" class="form-control"></div>
+                            </div>
+                            <div class="col-sm-6"><h4 class="m-t-none m-b">Select information <small>to be displayed.</small> </h4>
+                                    <div class="form-group"><label>Information</label> <input type="email" placeholder="Enter email" class="form-control"></div>
                                     <div>
                                         <button class="btn btn-sm btn-primary pull-right m-t-n-xs" type="submit"><strong>Log in</strong></button>
-                                        <label> <input type="checkbox" class="i-checks"> Remember me </label>
                                     </div>
                                 </form>
-                            </div>
-                            <div class="col-sm-6"><h4>Not a member?</h4>
-                                <p>You can create an account:</p>
-                                <p class="text-center">
-                                    <a href=""><i class="fa fa-sign-in big-icon"></i></a>
-                                </p>
                             </div>
                         </div>
                     </div>
