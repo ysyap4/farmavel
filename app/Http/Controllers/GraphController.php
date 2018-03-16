580100f91f874091f2ab9e8ef43f390413913523
Yap <yoongseng_yap@outlook.com>
2018-03-16 22:36:02 +0800

    make it better

--- app/Http/Controllers/GraphController.php
+++ app/Http/Controllers/GraphController.php
@@ -122,6 +122,17 @@ class GraphController extends Controller
         return View::make('graph_periodic_index', array('lastest_user' => $lastest_user, 'lastest_med' => $lastest_med));
     }
 
+    public function graph_periodic_results()
+    {
+        $lastest_user = users::orderBy('created_at', 'desc')->first();
+        $lastest_med = medicine::orderBy('created_at', 'desc')->first();
+
+        $get_slider = Input::get('get_slider');
+        dd($get_slider);
+
+        return View::make('graph_periodic_results', array('lastest_user' => $lastest_user, 'lastest_med' => $lastest_med));
+    }
+
     public function manage_user_index()
     {
         $user = users::all();
