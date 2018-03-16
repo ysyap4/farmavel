60a6534d3430d8c5e90d3477fadddcfcd576d657
Yap <yoongseng_yap@outlook.com>
2018-03-16 15:03:31 +0800

    make it better

--- resources/views/graph_periodic_index.blade.php
+++ resources/views/graph_periodic_index.blade.php
@@ -174,20 +174,11 @@
             <div class="col-lg-7">
                 <div class="ibox float-e-margins">
                     <div class="ibox-title">
-                        <h5>Basic form <small>Simple login form example</small></h5>
+                        <h5> Periodic Charts </h5>
                         <div class="ibox-tools">
                             <a class="collapse-link">
                                 <i class="fa fa-chevron-up"></i>
                             </a>
-                            <a class="dropdown-toggle" data-toggle="dropdown" href="#">
-                                <i class="fa fa-wrench"></i>
-                            </a>
-                            <ul class="dropdown-menu dropdown-user">
-                                <li><a href="#">Config option 1</a>
-                                </li>
-                                <li><a href="#">Config option 2</a>
-                                </li>
-                            </ul>
                             <a class="close-link">
                                 <i class="fa fa-times"></i>
                             </a>
@@ -195,8 +186,7 @@
                     </div>
                     <div class="ibox-content">
                         <div class="row">
-                            <div class="col-sm-6 b-r"><h3 class="m-t-none m-b">Sign in</h3>
-                                <p>Sign in today for more expirience.</p>
+                            <div class="col-sm-6 b-r"><h3 class="m-t-none m-b">Select information <small>to be displayed.</small> </h3>
                                 <form role="form">
                                     <div class="form-group"><label>Email</label> <input type="email" placeholder="Enter email" class="form-control"></div>
                                     <div class="form-group"><label>Password</label> <input type="password" placeholder="Password" class="form-control"></div>
