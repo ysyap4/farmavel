0f5c76d2b456576293de71f7a3dd875512fcbd26
Yap <yoongseng_yap@outlook.com>
2018-03-17 15:39:03 +0800

    make it better

--- resources/views/graph_periodic_index.blade.php
+++ resources/views/graph_periodic_index.blade.php
@@ -190,7 +190,7 @@
                     <div class="ibox-content">
                         <div class="row">
                             <div class="col-sm-6 b-r"><h4 class="m-t-none m-b">Select information <small>to be displayed.</small> </h4>
-                                <form role="form" method="GET" name="get_slider">
+                                <form role="form" method="GET" action="{{URL::route('graph_periodic_results')}}" name="get_slider" onsubmit="return get_slider();" id="get_slider">
                                     <div class="form-group"><label>Information</label> 
                                         <div class="radio i-checks"><label> <input type="radio" value="Report" name="information" checked=""> <i></i> Report </label></div>
                                         <div class="radio i-checks"><label> <input type="radio" value="Appointment" name="information"> <i></i> Appointment </label></div>
@@ -279,8 +279,22 @@
 
         var min = dragSlider.noUiSlider.get()[0];
         var max = dragSlider.noUiSlider.get()[1];
-        $("#get_slider_value1").val(min);
-        $("#get_slider_value2").val(max);
+        
+        function get_slider()
+        {
+            var form = $("#get_slider");
+            
+            $.ajax({
+    
+                    type:"GET",
+                    url:"{{URL::route('graph_periodic_results')}}/"+min+"/"+max,
+                    success: function(data) {
+                        console.log("Value added");
+                    }
+                });
+
+            return false;
+        }
         
     </script>
 
