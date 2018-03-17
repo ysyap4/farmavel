55cdd6b8a69569b6e1ec1f25da2f0d4441d318a4
Yap <yoongseng_yap@outlook.com>
2018-03-17 23:03:15 +0800

    make it better

--- resources/views/graph_periodic_index.blade.php
+++ resources/views/graph_periodic_index.blade.php
@@ -281,8 +281,8 @@
 
         function get_slider_value()
         {
-            document.getElementById("get_slider_value1").value = dragSlider.noUiSlider.get()[0];
-            document.getElementById("get_slider_value2").value = dragSlider.noUiSlider.get()[1];
+            document.getElementById("get_slider_value1").value = (dragSlider.noUiSlider.get()[0]);
+            document.getElementById("get_slider_value2").value = (dragSlider.noUiSlider.get()[1]);
             document.get_slider.action = "{{URL::route('graph_periodic_results')}}";
             document.get_slider.submit();
         }
