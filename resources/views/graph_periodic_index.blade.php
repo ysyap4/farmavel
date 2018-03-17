7c2c85de05a0dc57c87c5eb1c76c7f7bcc15c14c
Yap <yoongseng_yap@outlook.com>
2018-03-17 17:15:46 +0800

    make it better

--- resources/views/graph_periodic_index.blade.php
+++ resources/views/graph_periodic_index.blade.php
@@ -190,7 +190,7 @@
                     <div class="ibox-content">
                         <div class="row">
                             <div class="col-sm-6 b-r"><h4 class="m-t-none m-b">Select information <small>to be displayed.</small> </h4>
-                                <form role="form" method="GET" action="{{URL::route('graph_periodic_results')}}" name="get_slider" onsubmit="return get_slider();" id="get_slider">
+                                <form role="form" method="GET" name="get_slider" onClick="get_slider()" id="get_slider">
                                     <div class="form-group"><label>Information</label> 
                                         <div class="radio i-checks"><label> <input type="radio" value="Report" name="information" checked=""> <i></i> Report </label></div>
                                         <div class="radio i-checks"><label> <input type="radio" value="Appointment" name="information"> <i></i> Appointment </label></div>
@@ -287,7 +287,7 @@
             var a = min.val();
             var b = max.val();
 
-            $.ajax({
+            jQuery.ajax({
     
                     type:"GET",
                     url:"{{URL::route('graph_periodic_results')}}",
