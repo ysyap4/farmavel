4811526ae865ca76a22ad744a317aedfb5c3115f
Yap <yoongseng_yap@outlook.com>
2018-03-18 15:41:19 +0800

    make it better

--- app/Http/Controllers/GraphController.php
+++ app/Http/Controllers/GraphController.php
@@ -140,6 +140,31 @@ class GraphController extends Controller
             {
                 $month[$i] = '';
                 $month[$i] = $start_month + $i;
+
+                if ($month[$i] = 1)
+                    $month[$i] = "Jan";
+                else if ($month[$i] = 2)
+                    $month[$i] = "Feb";
+                else if ($month[$i] = 3)
+                    $month[$i] = "Mar";
+                else if ($month[$i] = 4)
+                    $month[$i] = "Apr";
+                else if ($month[$i] = 5)
+                    $month[$i] = "May";
+                else if ($month[$i] = 6)
+                    $month[$i] = "Jun";
+                else if ($month[$i] = 7)
+                    $month[$i] = "Jul";
+                else if ($month[$i] = 8)
+                    $month[$i] = "Aug";
+                else if ($month[$i] = 9)
+                    $month[$i] = "Sep";
+                else if ($month[$i] = 10)
+                    $month[$i] = "Oct";
+                else if ($month[$i] = 11)
+                    $month[$i] = "Nov";
+                else if ($month[$i] = 12)
+                    $month[$i] = "Dec";
             }
 
         return View::make('graph_periodic_results', array('lastest_user' => $lastest_user, 'lastest_med' => $lastest_med, 'report' => $report, 'appointment' => $appointment, 'month' => $month, 'display_results' => $display_results));
