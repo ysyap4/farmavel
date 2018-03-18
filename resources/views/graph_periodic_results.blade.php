d674e0a6acb383b4f46f717fb428489ed7280da2
Yap <yoongseng_yap@outlook.com>
2018-03-18 16:27:28 +0800

    make it better

--- resources/views/graph_periodic_results.blade.php
+++ resources/views/graph_periodic_results.blade.php
@@ -389,6 +389,14 @@
                         highlightFill: "rgba(26,179,148,0.75)",
                         highlightStroke: "rgba(26,179,148,1)",
                         data: [28, 48, 40, 19, 86, 27, 90]
+                    },
+                    {
+                        label: "My First dataset",
+                        fillColor: "rgba(220,220,220,0.5)",
+                        strokeColor: "rgba(220,220,220,0.8)",
+                        highlightFill: "rgba(220,220,220,0.75)",
+                        highlightStroke: "rgba(220,220,220,1)",
+                        data: [65, 59, 80, 81, 56, 55, 40]
                     }
                 ]
             };
