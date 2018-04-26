b9934520649e1faff48b1bb26516f53af489cd62
Yap <yoongseng_yap@outlook.com>
2018-04-26 15:39:12 +0800

    make it better

--- resources/views/manage_report_index.blade.php
+++ resources/views/manage_report_index.blade.php
@@ -332,6 +332,13 @@
                 "New row" ] );
 
         }
+
+        $(document).ready(function () {
+            $('.i-checks').iCheck({
+                checkboxClass: 'icheckbox_square-green',
+                radioClass: 'iradio_square-green',
+            });
+        });
     </script>
 
     <script type="text/javascript">
