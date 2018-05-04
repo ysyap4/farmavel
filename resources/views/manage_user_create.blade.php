64b43fa6a1b4d7234fd192150ea3400eec5a45cf
Yap <yoongseng_yap@outlook.com>
2018-05-04 17:13:35 +0800

    make it better

--- resources/views/manage_user_create.blade.php
+++ resources/views/manage_user_create.blade.php
@@ -354,8 +354,6 @@
             });
         });
 
-        
-
         function readURL(input) {
             if (input.files && input.files[0]) {
                 var reader = new FileReader();
@@ -370,23 +368,32 @@
             readURL(this);
         });
 
-        $("#download").click(function() {
-            window.open($image.cropper("getDataURL"));
-        });
-        $("#zoomIn").click(function() {
-            $image.cropper("zoom", 0.1);
-        });
-        $("#zoomOut").click(function() {
-            $image.cropper("zoom", -0.1);
-        });
-        $("#rotateLeft").click(function() {
-            $image.cropper("rotate", 90);
-        });
-        $("#rotateRight").click(function() {
-            $image.cropper("rotate", -90);
-        });
-        $("#setDrag").click(function() {
-            $image.cropper("setDragMode", "crop");
+        $(document).ready(function() {
+            $('#inputImage').each(function() {
+            var maxWidth = 100; // Max width for the image
+            var maxHeight = 100;    // Max height for the image
+            var ratio = 0;  // Used for aspect ratio
+            var width = $(this).width();    // Current image width
+            var height = $(this).height();  // Current image height
+        
+            // Check if the current width is larger than the max
+            if(width > maxWidth){
+                ratio = maxWidth / width;   // get ratio for scaling image
+                $(this).css("width", maxWidth); // Set new width
+                $(this).css("height", height * ratio);  // Scale height based on ratio
+                height = height * ratio;    // Reset height to match scaled image
+            }
+        
+            var width = $(this).width();    // Current image width
+            var height = $(this).height();  // Current image height
+        
+            // Check if current height is larger than max
+            if(height > maxHeight){
+                ratio = maxHeight / height; // get ratio for scaling image
+                $(this).css("height", maxHeight);   // Set new height
+                $(this).css("width", width * ratio);    // Scale width based on ratio
+                width = width * ratio;    // Reset width to match scaled image
+            }
         });
     </script>
 
