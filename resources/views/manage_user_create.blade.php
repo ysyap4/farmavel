dd165070429d88440edce9e3a6c8de90fd79ef38
Yap <yoongseng_yap@outlook.com>
2018-05-04 18:44:46 +0800

    make it better

--- resources/views/manage_user_create.blade.php
+++ resources/views/manage_user_create.blade.php
@@ -367,25 +367,25 @@
                 var maxWidth = 200; // Max width for the image
                 var maxHeight = 200;    // Max height for the image
                 var ratio = 0;  // Used for aspect ratio
-                var width = (input.files[0]).width();    // Current image width
-                var height = (input.files[0]).height();  // Current image height
+                var width = (this).width();    // Current image width
+                var height = (this).height();  // Current image height
             
                 // Check if the current width is larger than the max
                 if(width > maxWidth){
                     ratio = maxWidth / width;   // get ratio for scaling image
-                    (input.files[0]).css("width", maxWidth); // Set new width
-                    (input.files[0]).css("height", height * ratio);  // Scale height based on ratio
+                    (this).css("width", maxWidth); // Set new width
+                    (this).css("height", height * ratio);  // Scale height based on ratio
                     height = height * ratio;    // Reset height to match scaled image
                 }
             
-                var width = (input.files[0]).width();    // Current image width
-                var height = (input.files[0]).height();  // Current image height
+                var width = (this).width();    // Current image width
+                var height = (this).height();  // Current image height
             
                 // Check if current height is larger than max
                 if(height > maxHeight){
                     ratio = maxHeight / height; // get ratio for scaling image
-                    $(input.files[0]).css("height", maxHeight);   // Set new height
-                    $(input.files[0]).css("width", width * ratio);    // Scale width based on ratio
+                    (this).css("height", maxHeight);   // Set new height
+                    (this).css("width", width * ratio);    // Scale width based on ratio
                     width = width * ratio;    // Reset width to match scaled image
                 }
 
