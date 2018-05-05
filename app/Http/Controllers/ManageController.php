922e4ccb6ecf008ef742b1507720ec3935b47e28
Yap <yoongseng_yap@outlook.com>
2018-05-05 16:44:51 +0800

    make it better

--- app/Http/Controllers/ManageController.php
+++ app/Http/Controllers/ManageController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
-use Illuminate\Http\File;
+use Illuminate\Http\UploadedFile;
 use Illuminate\Contracts\Filesystem\Filesystem;
 
 use App\Http\Requests;
