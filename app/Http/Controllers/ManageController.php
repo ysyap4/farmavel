c1c987d798ee6280a6d3e3ca925ec02ad66fb3f0
Yap <yoongseng_yap@outlook.com>
2018-05-05 16:38:40 +0800

    make it better

--- app/Http/Controllers/ManageController.php
+++ app/Http/Controllers/ManageController.php
@@ -94,7 +94,7 @@ class ManageController extends Controller
                 $destinationPath = public_path().'/user_image/';
                 $image->move($destinationPath, $save_image_name);
                 $path = "user_image/".$save_image_name;                
-                Storage::disk('s3')->put($path, file_get_contents($image));
+                Storage::disk('s3')->put('user_image', $image);
 
                 //$add->image = $save_image_name;
                 users::where('id', $add->id)->update(['image' => $save_image_name]);
