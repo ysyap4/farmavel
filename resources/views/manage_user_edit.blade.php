5882e8c42d7debb3224fee90172de06db638d65c
Yap <yoongseng_yap@outlook.com>
2018-05-06 23:52:43 +0800

    make it better

--- resources/views/manage_user_edit.blade.php
+++ resources/views/manage_user_edit.blade.php
@@ -289,15 +289,15 @@
                                         <div class="col-md-6">
                                             <div class="image-crop">
                                                 @if (is_null($edit_selected_user[$i]->image))
-                                                    <img src="{{URL::asset('user_image/no_image.png')}}" id="change_image{{[$i]}}" style="height: 200px; width: 200px;">
+                                                    <img src="{{URL::asset('user_image/no_image.png')}}" id="change_image{{$i}}" style="height: 200px; width: 200px;">
                                                 @else
-                                                    <img src="{{URL::asset(Storage::disk('s3')->url('user_image/' . $edit_selected_user[$i]->image))}}" id="change_image" style="height:200px; width:200px;">
+                                                    <img src="{{URL::asset(Storage::disk('s3')->url('user_image/' . $edit_selected_user[$i]->image))}}" id="change_image{{$i}}" style="height:200px; width:200px;">
                                                 @endif
                                             </div>
                                             <br>
                                             <div class="btn-group">
                                                 <label title="Upload image file" for="inputImage" class="btn btn-primary">
-                                                    <input type="file" accept="image/*" name="image[]" value=" " id="inputImage" class="hide">
+                                                    <input type="file" accept="image/*" name="image[]" value=" " id="inputImage{{$i}}" class="hide">
                                                     Upload new image
                                                 </label>
                                             </div>
