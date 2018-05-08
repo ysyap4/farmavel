cee16cfeb456700a1afe2304f580f2f942e0bba3
Yap <yoongseng_yap@outlook.com>
2018-05-08 16:37:44 +0800

    make it better

--- app/Http/Controllers/ManageController.php
+++ app/Http/Controllers/ManageController.php
@@ -209,7 +209,7 @@ class ManageController extends Controller
 
                 $file = $image[$i];
 
-                if($file->isValid())
+                if($file->isValid() && !is_null($file))
                 {
                     $image_filename[$i] = $file->getClientOriginalName();
                     $image_extension[$i] = $file->getClientOriginalExtension();
@@ -234,7 +234,7 @@ class ManageController extends Controller
                 }
                 else
                 {
-                    
+
                 }
             }
 
