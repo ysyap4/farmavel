6479eee7313dddf4dbb2beb880ee664f21a187b3
Yap <yoongseng_yap@outlook.com>
2018-05-14 17:43:59 +0800

    make it better

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -69,7 +69,7 @@ class RegisterController extends Controller
             'phone' => $data['phone'],
             'password' => bcrypt($data['password']),
             'type' => 'Patient',
-            'image' => $data['image'],
+            //'image' => $data['image'],
         ]);
     }
 }
